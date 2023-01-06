d3a6b5d5f09a9fb11868302d8b36802ebf9645ca
Stefan Caras <stefan.caras@gmail.com>
2023-01-06 16:40:46 +0200

    rewrite

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 const container = document.querySelector('#container');
 const input = document.querySelector('#input');
-const buttonAdd = document.querySelector('#buttonAdd');
 const list = document.querySelector('#list');
+
+const sortListChildren = (n1, n2) => {
+    [...list.children]
+        .sort((a, b) => a.textContent > b.textContent ? n1 : n2)
+        .forEach(node => list.appendChild(node));
+}
 
 input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-      buttonAdd.click();
+        document.querySelector('#btn-add').click();
     }
 });
 
-container.addEventListener('click', (event) => {
-    const clickedElement = event.target;
-    if (clickedElement.id === 'buttonAdd' && input.value != "") {
-        let array = input.value.split(',');
+container.addEventListener('click', (e) => {
+    if (e.target.id === 'btn-add' && input.value !== "") {
+        const array = input.value.split(/[,<>]/);
         array.forEach(el => {
-            if (el != "") {
-                const div = document.createElement('div');
-                const button = document.createElement('button');
-                div.classList.add('item');
-                div.textContent = el.trim().toLowerCase();
-                button.classList.add('buttonStrike');
-                button.textContent = 'Taie';
-                div.append(button);
-                list.prepend(div);
+            if (el !== "") {
+                list.innerHTML += `
+                    <div class="item">
+                        <span>${el.trim().toLowerCase()}</span>
+                        <button class="btn-strike">Taie</button>
+                    </div>`
                 input.value = '';
             }
         });
-    } else if (clickedElement.classList.contains('buttonSU')) {
-        [...list.children]
-        .sort((a,b) => a.textContent > b.textContent ? 1 : -1)
-        .forEach(node => list.appendChild(node));
-    } else if (clickedElement.classList.contains('buttonSD')) {
-        [...list.children]
-        .sort((a,b) => a.textContent < b.textContent ? 1 : -1)
-        .forEach(node => list.appendChild(node));
-    } else if (clickedElement.id === 'buttonDelete') {
-        const items = document.querySelectorAll('.item');
-        items.forEach(item => {item.remove()});
-    } else if (clickedElement.classList.contains('buttonStrike')) {
-        clickedElement.parentNode.classList.toggle('strike');
-        clickedElement.textContent = '';
-        if (!clickedElement.parentNode.classList.contains('strike')) {
-            clickedElement.textContent = 'Taie';
+    } else if (e.target.classList.contains('btn-sort-up')) {
+        sortListChildren(1, -1);
+    } else if (e.target.classList.contains('btn-sort-down')) {
+        sortListChildren(-1, 1);
+    } else if (e.target.id === 'btn-new-list') {
+        list.innerHTML = "";
+    } else if (e.target.classList.contains('btn-strike')) {
+        e.target.parentNode.classList.toggle('bg-dark');
+        e.target.parentNode.childNodes[1].classList.toggle('strike');
+        e.target.classList.toggle('bg-white');
+        e.target.textContent = 'Revino';
+        if (!e.target.parentNode.classList.contains('bg-dark')) {
+            e.target.textContent = 'Taie';
         }
     }
 });
